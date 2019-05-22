f88a1a732ba14235842cf9b294ef7a9c7053d0c2
ckd1618 <ckd1618@gmail.com>
2019-05-21 18:56:19 -0700

    working email without pass

--- poofy/settings.py
+++ poofy/settings.py
@@ -151,4 +151,4 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = '587'
 EMAIL_USE_TLS = True
 EMAIL_HOST_USER = 'ckd1240@gmail.com'
-EMAIL_HOST_PASSWORD = 'cchrissdd'#enter your password here as you setup the server
+EMAIL_HOST_PASSWORD = ''#enter your password here as you setup the server
